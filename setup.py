acea8a78256b372b5e526d2c265ee4aa95343f4f
mohd2548 <mohdsahila132@gmail.com>
2025-02-16 03:16:10 +0530

    This is the setup commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='ML_Project',
+    version='0.0.1',
+    author='Mohd Sahil',
+    author_email='mohdsahila132@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn'],
+
+)
